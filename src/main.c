e64d064a33a7e432d9df19bc71aa30dc05bdf4d3
VikasHaritwal93 <118720615+VikasHaritwal93@users.noreply.github.com>
2023-11-29 13:22:23 +0530

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,6 +1,11 @@
+// main.c
 #include <stdio.h>
 
+int add(int a, int b) {
+    return a + b;
+}
+
 int main() {
-    printf("Hello, World!\n");
+    printf("Result: %d\n", add(3, 4));
     return 0;
 }
